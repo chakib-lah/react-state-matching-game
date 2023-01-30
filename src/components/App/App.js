58ceee2a20496667c6a95e2e4c80acec854ce3a7
LAHNAKI Chakib <chakib.lahnaki@gmail.com>
2023-01-30 17:55:31 +0100

    handling click events

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import {createTiles} from '../../misc/utils';
+import {createTiles, indexOfSelected} from '../../misc/utils';
 
 import './App.css';
 
@@ -19,12 +19,45 @@ class App extends Component {
         }
     }
 
+    handleTileClicked(id, color) {
+        this.setState((state) => {
+            const tiles = state.tiles;
+            let toBeCleared = state.toBeCleared;
+            const selectedTileIndex = indexOfSelected(tiles, id, color);
+            let previousTileIndex = state.previousTileIndex;
+
+            if(toBeCleared !== null) {
+                tiles[toBeCleared[0]].selected = false
+                tiles[toBeCleared[1]].selected = false
+                toBeCleared = null
+            }
+
+            tiles[selectedTileIndex].selected = true
+
+            if (previousTileIndex !== null) {
+                const previousTile = tiles[previousTileIndex];
+                if (previousTile.id !== selectedTile.id && previousTile.color === color) {
+                    selectedTile.matched = true
+                    previousTile.matched = true
+                    previousTileIndex = null
+                } else {
+                    toBeCleared = [previousTileIndex, selectedTileIndex]
+                    previousTileIndex = null
+                }
+            } else {
+                previousTileIndex = selectedTileIndex;
+            }
+            return {toBeCleared, tiles, previousTileIndex}
+        })
+
+    }
+
     startGame = (numTiles) => {
-        this.setState((state)=> ({
-            playing : true,
+        this.setState((state) => ({
+            playing: true,
             previousTileIndex: null,
             toBeCleared: null,
-            tiles: createTiles(state.numTiles)
+            tiles: createTiles(state.numTiles, this.handleTileClicked)
         }))
     }
 
@@ -34,8 +67,8 @@ class App extends Component {
                 <header className="App-header">
                     Turbo-Matcher
                 </header>
-                <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
-                <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+                <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}/>
+                <Board numTiles={this.state.numTiles} tiles={this.state.tiles}/>
                 }
             </div>
         );
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,11 +3,13 @@ import React from 'react'
 import './Tile.css'
 
 const Tile = (props) => {
-    const dynamicColor  = props.selected || props.matched ? { backgroundColor: props.color } : null;
+    const dynamicColor = props.selected || props.matched ? {backgroundColor: props.color} : null;
 
     return (
-        <div className='Tile' style={dynamicColor }>
-            { props.selected || props.matched ? <props.svg/> : null }
+        <div className='Tile' style={dynamicColor} onClick={() => {
+            props.handleTileClicked(props.id, props.color)
+        }}>
+            {props.selected || props.matched ? <props.svg/> : null}
         </div>
     )
 }
