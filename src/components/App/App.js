2a8fce44179474bf5cb02aae772887fb4147b231
LAHNAKI Chakib <chakib.lahnaki@gmail.com>
2023-01-30 18:28:40 +0100

    create custom hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -53,6 +53,14 @@ class App extends Component {
 
     }
 
+    handleNumTileChange = (num) => {
+        this.setState({
+            numTiles: num,
+            playing: false,
+            tiles: []
+        })
+    }
+
     startGame = (numTiles) => {
         this.setState((state) => ({
             playing: true,
@@ -68,7 +76,8 @@ class App extends Component {
                 <header className="App-header">
                     Turbo-Matcher
                 </header>
-                <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}/>
+                <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}
+                              handleNumTileChange={this.handleNumTileChange}/>
                 <Board numTiles={this.state.numTiles} tiles={this.state.tiles}/>
                 }
             </div>
